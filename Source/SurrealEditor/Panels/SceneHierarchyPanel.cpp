acec851291edb4eec1aac05382aa33f336d444de
bishoppokhrel <pokhrelbishop@gmail.com>
2026-03-02 20:38:18 +0545

    WIP Scene Manager Panel

--- Source/SurrealEditor/Panels/SceneHierarchyPanel.cpp
+++ Source/SurrealEditor/Panels/SceneHierarchyPanel.cpp
@@ -11,8 +11,108 @@ namespace SurrealStudio {
 			return true; 
 		}
 
-		bool SceneHierarchyPanel::DrawCommandPlattle()
+		bool SceneHierarchyPanel::DrawCommandPalette()
 		{
+			if (m_OpenCommandPalttleDialogBox)
+			{
+				ImGui::OpenPopup("Command Palette");
+				m_OpenCommandPalttleDialogBox = false;
+			}
+
+			if (ImGui::BeginPopupModal("Command Palette", nullptr, ImGuiWindowFlags_AlwaysAutoResize))
+			{
+				if (ImGui::BeginMenu("Add..."))
+				{
+					if (ImGui::MenuItem("Add Object"))
+					{
+						if (m_ObjectManager.m_Objects.size() > ECS::MAX_OBJECTS && m_SceneHierarchyPanelAdditionalDataNeeded.objectDataNeeded.openMaxObjectsReached_PerWorld_SSERROR_DialogBox_SHP)
+						{
+							ImGui::OpenPopup("[SS ERROR] Max amount of Objects reached per World");
+							m_SceneHierarchyPanelAdditionalDataNeeded.objectDataNeeded.openMaxObjectsReached_PerWorld_SSERROR_DialogBox_SHP = false;
+						}
+
+						if (ImGui::BeginPopupModal("[SS ERROR] Max amount of Objects reached per World", nullptr, ImGuiWindowFlags_AlwaysAutoResize))
+						{
+							ImGui::Text("Max amount of Objects per World is %d, got %d.", ECS::MAX_OBJECTS, static_cast<int>(m_ObjectManager.m_Objects.size()));
+
+							if (ImGui::Button("OK")) {
+								ImGui::CloseCurrentPopup();
+								m_ObjectManager.m_Objects.resize((size_t)ECS::MAX_OBJECTS); // Clamp to the max value ig
+								m_SceneHierarchyPanelAdditionalDataNeeded.objectDataNeeded.openAddObjectNamePopupDialogBox = true; // Reset
+							}
+						}
+
+						m_SceneHierarchyPanelAdditionalDataNeeded.objectDataNeeded.openAddObjectNamePopupDialogBox = true; 
+						if (m_SceneHierarchyPanelAdditionalDataNeeded.objectDataNeeded.openAddObjectNamePopupDialogBox)
+						{
+							ImGui::OpenPopup("Object Name...");
+							m_SceneHierarchyPanelAdditionalDataNeeded.objectDataNeeded.openAddObjectNamePopupDialogBox = false;
+						}
+
+						if (ImGui::BeginPopupModal("Object Name...", nullptr, ImGuiWindowFlags_AlwaysAutoResize))
+						{
+							ImGui::InputText("Enter the Object Name...",
+								m_SceneHierarchyPanelAdditionalDataNeeded.objectDataNeeded.objToAdd_NameRequiredBuffer,
+								sizeof(m_SceneHierarchyPanelAdditionalDataNeeded.objectDataNeeded.objToAdd_NameRequiredBuffer));
+
+							if (ImGui::Button("OK"))
+							{
+								ImGui::CloseCurrentPopup();
+								std::string objectName = m_SceneHierarchyPanelAdditionalDataNeeded.objectDataNeeded.objToAdd_NameRequiredBuffer;
+								m_ObjectManager.CreateObject(objectName);
+							}
+
+							if (ImGui::Button("Cancel"))
+							{
+								ImGui::CloseCurrentPopup();
+							}
+
+							ImGui::EndPopup();
+						}
+					}
+
+					if (ImGui::BeginMenu("Add Component"))
+					{
+						if (ImGui::MenuItem("Add Transform Component"))
+						{
+							m_SceneHierarchyPanelAdditionalDataNeeded.componentDataNeeded.initalTransformComponentPropertiesToBeFilled = true; 
+							if (m_SceneHierarchyPanelAdditionalDataNeeded.componentDataNeeded.initalTransformComponentPropertiesToBeFilled)
+							{
+								ImGui::OpenPopup("Initial Transform Component Properties");
+								m_SceneHierarchyPanelAdditionalDataNeeded.componentDataNeeded.initalTransformComponentPropertiesToBeFilled = false;
+							}
+
+							if (ImGui::BeginPopupModal("Initial Transform Component Properties", nullptr, ImGuiWindowFlags_AlwaysAutoResize))
+							{
+								ImGui::InputText("Object Name (which Object the Component should be a child of)", 
+									m_SceneHierarchyPanelAdditionalDataNeeded.componentDataNeeded.objectNameRequiredForComponentCreation, 
+									sizeof(m_SceneHierarchyPanelAdditionalDataNeeded.componentDataNeeded.objectNameRequiredForComponentCreation));
+
+								if (ImGui::Button("OK"))
+								{
+									ImGui::CloseCurrentPopup();
+									std::string objectNameToBeAChildOfComponent = m_SceneHierarchyPanelAdditionalDataNeeded.componentDataNeeded.objectNameRequiredForComponentCreation;
+									ECS::ObjectData objectDataToBePassedInForComponentCreation;
+									objectDataToBePassedInForComponentCreation.name = objectNameToBeAChildOfComponent;
+									// No need to fill other properties
+									m_ComponentManager.transformComponentManager.AddTransformComponent(objectDataToBePassedInForComponentCreation, { 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 0.0f }, {0.0f, 0.0f, 0.0f});
+								}
+							}
+						}
+
+						if (ImGui::MenuItem("Add Physics Component"))
+						{
+
+						}
+
+						ImGui::EndMenu();
+					}
+
+					ImGui::EndMenu();
+				}
+				ImGui::EndPopup();
+			}
+
 			return true;
 		}
 
@@ -23,7 +123,7 @@ namespace SurrealStudio {
 			DrawSceneHierarchy();
 
 			if (m_InputSystem.IsRightMouseButtonPressed(m_Window))
-				DrawCommandPlattle();
+				DrawCommandPalette();
 
 			ImGui::End();
 			return true;
--- Source/SurrealEditor/Panels/SceneHierarchyPanel.h
+++ Source/SurrealEditor/Panels/SceneHierarchyPanel.h
@@ -1,23 +1,62 @@
 #pragma once
 
 #include <Engine/InputSystem.h>
+#include <Engine/ECS/Object.h>
+#include <Engine/ECS/Component.h>
+
+#include <Engine/Scene/Subscene.h>
+#include <Engine/Scene/World.h>
 
 namespace SurrealStudio {
 
 	namespace SurrealEditor {
+
+		struct SceneHierarchyPanelAdditionalDataNeeded
+		{
+			// Buffer needed for ImGui::InputText (Add Object, ImGui::MenuItem())
+			struct ObjectDataNeeded
+			{
+				char objToAdd_NameRequiredBuffer[512];
+				bool openAddObjectNamePopupDialogBox = false; // To open an ImGui popup to add a name for the Object.
+				bool openMaxObjectsReached_PerWorld_SSERROR_DialogBox_SHP = false; // To prevent mulitple variables having the same name
+			};
+
+			struct ComponentDataNeeded
+			{
+				char objectNameRequiredForComponentCreation[512]; // To ensure that a Component is at least a child of an Object.
+				bool initalTransformComponentPropertiesToBeFilled = false; 
+				
+
+			};
+
+			ComponentDataNeeded componentDataNeeded;
+			ObjectDataNeeded objectDataNeeded;
+		};
 
 		class SceneHierarchyPanel
 		{
 		public:
 
 			bool DrawSceneHierarchy();
-			bool DrawCommandPlattle();  // Trigged when right clicked
+			bool DrawCommandPalette();  // Trigged when right clicked
 			bool DrawSceneHierarchyPanel();
 
 		private:
 
+			SceneHierarchyPanelAdditionalDataNeeded m_SceneHierarchyPanelAdditionalDataNeeded;
 			InputSystem m_InputSystem;
 			GLFWwindow* m_Window; 
+
+			bool m_OpenCommandPalttleDialogBox = false; 
+
+			// ECS/Scene System classes instances
+			ECS::ObjectData m_ObjectData; 
+			ECS::ObjectManager m_ObjectManager;
+			ECS::ComponentManager m_ComponentManager;
+
+			SceneSystem::Subscene m_Subscene;
+			SceneSystem::World m_World;
+			SceneSystem::WorldData m_WorldData;
 		};
 	}
 }
